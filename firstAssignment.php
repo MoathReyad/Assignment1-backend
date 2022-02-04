064954991d469064984195e8e59bd73436a4b959
MoathReyad <96656691+MoathReyad@users.noreply.github.com>
2022-02-04 14:41:24 +0200

    Add files via upload

---
+++ firstAssignment.php
@@ -0,0 +1,20 @@
+<!-- Hello everyone Please send me a picture for XAMPP control panel window to make sure it works for everyone ,
+then you should create an php file and write a simple code for these questions
+
+Write a program to print "Hello PHP" using php variable?
+
+Write a program to print 2 php variables using single echo statement?
+
+Write a program to print title and paragraph using html tags inside php script? -->
+
+<?php
+    echo "<title>First Assignment</title>";
+    echo "<p>Welcome to our site</p>";
+
+    $x = "Hello PHP";
+    echo $x . "<br>";
+
+    $y = 10;
+    $z = 13;
+    echo "sum Y and Z equl " . ($y + $z);
+?>
